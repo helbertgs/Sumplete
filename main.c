3c718192644c2ab4bbe885b0aaf3b3faac6f0ea3
Helbert Gomes <helbertgs@gmail.com>
2023-08-20 22:46:05 -0400

    chore: update the printBoard function to print the board and hints

--- main.c
+++ main.c
@@ -4,15 +4,20 @@
 
 typedef struct Sumplete Sumplete;
 struct Sumplete {
-    int size;
     int **board;
+    int *cols;
+    int *rows;
+    int size;
 };
 
 Sumplete init(int size) {
     Sumplete sumplete;
-    sumplete.size = size;
-    sumplete.board = (int **)malloc(size * sizeof(int *));
 
+    sumplete.board = (int **)malloc(size * sizeof(int *));
+    sumplete.cols = (int *)malloc(size * sizeof(int *));
+    sumplete.rows = (int *)malloc(size * sizeof(int *));
+    sumplete.size = size;
+    
     for (int row = 0; row < size; row++) {
         sumplete.board[row] = (int *)malloc(size * sizeof(int *));
     }
@@ -25,5 +30,18 @@ void printBoard(Sumplete *sumplete) {
         for (int col = 0; col < sumplete->size; col++) {
             printf("%d ", sumplete->board[row][col]);
         }
+
+        printf("| %d\n", sumplete->rows[row]);
     }
+
+    for (int i = 0; i < 2 * sumplete->size + 3; i++) {
+        printf("-");
+    }
+    print("\n");
+
+    for (int col = 0; col < sumplete->size; col++) {
+        printf("%d ", sumplete->cols[col]);
+    }
+
+    print("\n\n");
 }
